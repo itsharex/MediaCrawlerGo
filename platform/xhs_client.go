738383aeeae9bd6dda4583b0ba558cb37281bb57
Relakkes <relakkes@gmail.com>
2023-09-04 00:14:53 +0800

    feat: playwright-go support

---
+++ platform/xhs_client.go
@@ -0,0 +1,4 @@
+package platform
+
+type XhsClient struct {
+}
--- platform/xhs_core.go
+++ platform/xhs_core.go
@@ -16,24 +16,28 @@ func (xhs *ReadNoteCore) InitConfig(loginType string) {
 
 func (xhs *ReadNoteCore) Start() {
 	util.Log().Info("XhsReadNoteCore.Start called ...")
-
 	pw, err := playwright.Run()
 	if err != nil {
 		util.Log().Error("could not start playwright: %v", err)
 	}
-	browser, err := pw.Chromium.Launch()
-	if err != nil {
-		util.Log().Error("could not launch browser: %v", err)
-	}
-	page, err := browser.NewPage()
-	if err != nil {
-		util.Log().Error("could not create page: %v", err)
-	}
+	browser, _ := pw.Chromium.Launch(playwright.BrowserTypeLaunchOptions{
+		Headless: playwright.Bool(false),
+	})
+
+	//stealth.min.js is a js script to prevent the website from detecting the crawler.
+	page, _ := browser.NewPage()
+	filePath := "libs/stealth.min.js"
+	_ = page.AddInitScript(playwright.Script{Path: &filePath})
+
+	// goto xhs site
 	if _, err = page.Goto("https://www.xiaohongshu.com"); err != nil {
 		util.Log().Error("could not goto: %v", err)
 	}
 
 	xhs.search()
+
+	// block
+	select {}
 }
 
 func (xhs *ReadNoteCore) search() {
